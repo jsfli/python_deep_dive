95b69a183b51fb5e4d16a963c114235c711319e2
Siu Fung Jeffrey Li <jeffreyli@Sius-MacBook-Pro.local>
2020-11-03 15:31:08 +0800

    function commit

---
+++ part_1/chapter 2/6_functions.py
@@ -0,0 +1,25 @@
+from math import sqrt
+
+sqrt(3)
+import math
+
+PI = math.pi
+
+print(PI)
+
+print(math.exp(1))
+
+def func_1():
+    print('running func_1')
+
+func_1()
+
+def func_2(a: int, b: int):
+    print(a * b)
+
+func_2(2,3)
+
+def function_3(a,b):
+    print(a*b)
+
+function_3(4,5)
