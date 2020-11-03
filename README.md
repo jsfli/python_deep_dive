1ed983f49feab32a7b6ed9f0be23ae4a96606e69
Siu Fung Jeffrey Li <jeffreyli@Sius-MacBook-Pro.local>
2020-11-03 11:18:48 +0800

    2.4 variable_names initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # python_deep_dive
 ## Part 1
 ###2. A Quick Refresher - Basics Review
-2020.11.03 Chater 2_1,2,3
+2020.11.03 Chater 2_1,2,3,4
---
+++ part_1/chapter 2/4_variable_names.py
@@ -0,0 +1,5 @@
+# identifier names are case sensitive
+
+# conventions
+## must start with underscore or letter
+# var my_var index1 index_1 _var __var __it__ # are all legal names
