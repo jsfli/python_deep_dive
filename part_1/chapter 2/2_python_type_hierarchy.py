834dc622ab7dedafe5ea6fc598d40a3cce44583f
Siu Fung Jeffrey Li <jeffreyli@Sius-MacBook-Pro.local>
2020-11-03 10:32:33 +0800

    added multiline_statements_and_strings notes

--- part_1/chapter 2/2_python_type_hierarchy.py
+++ part_1/chapter 2/2_python_type_hierarchy.py
@@ -7,3 +7,23 @@
 
 # Sequences can be divided into Mutable (lists)
 # and immutable such as (Tuples and Strings)
+
+# Sets
+
+# Sets can also be devided into mutable Sets and Immutable Forzen Sets
+
+# Mappings
+
+# Dictionaries
+# Both sets and and dictionaires are basically hash maps
+
+# Callables
+
+# User-Defined Functions, Generators, Classes, Instance Methods
+# Class Instances (__call__())
+# Built-in Functions(e.g. len(), open())
+# Built-in Methods (e.g. my_list,append(x))
+
+# Singletons
+
+# None, Notimplemented, Ellipsis(...)
