e4dd3ad06cfc48093aeba1b3a94ab172806e7c7c
Siu Fung Jeffrey Li <jeffreyli@Sius-MacBook-Pro.local>
2020-11-03 14:39:22 +0800

    amended conditionals

---
+++ part_1/chapter 2/5_Conditionals.py
@@ -0,0 +1,12 @@
+a = 10
+
+if a < 5:
+    print('a < 5')
+elif a < 10:
+    print(' 5 <= a < 10')
+elif a < 15:
+    print('10 <= a < 15')
+elif a < 20:
+    print('20 <= a < 25')
+else:
+    print("a > 25")
