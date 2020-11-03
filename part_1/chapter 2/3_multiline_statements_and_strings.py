468094acedecd7e82e680399243bafcd50419ab7
Siu Fung Jeffrey Li <jeffreyli@Sius-MacBook-Pro.local>
2020-11-03 11:02:23 +0800

    end of multiline_statemnent_lecture git push

---
+++ part_1/chapter 2/3_multiline_statements_and_strings.py
@@ -0,0 +1,34 @@
+# Python Program are just
+
+# physical lines of code - end with a physical newline character
+# logical lines of codes - end with a logical NEWLINE token
+
+# physical newline are not necessarily the same as logical NEWLINE
+
+## most of the time, but not always
+
+### sometimes, physical newlines are ignored in order to combine
+### multiple physical lines into a single logical line of code
+### mainly for readability purposes
+
+## these are expressed implicitly or explicitly.
+
+## implicit expressions:
+### list literals: [], tuple (), dictionary {}, sets {}
+
+# tokenised
+
+## for string literals, I can use triple single quotes or double quotes
+''' This is a
+multi-line string '''
+
+"""
+
+"""
+
+a = [1,2,3]
+
+a = [1,
+     2,
+     3,
+     4]
