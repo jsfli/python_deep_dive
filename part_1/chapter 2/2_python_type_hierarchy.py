96cfb9f5abf10119ca25f49ab3c81a88988604d1
Siu Fung Jeffrey Li <jeffreyli@Sius-MacBook-Pro.local>
2020-11-03 10:06:56 +0800

    added chapter 2 folder

---
+++ part_1/chapter 2/2_python_type_hierarchy.py
@@ -0,0 +1,9 @@
+# Numbers
+
+# Intergral - includes Integers Booleans
+# Non-Integral - includes Floats, Complex, Decimals, Fractions.
+
+# Collections
+
+# Sequences can be divided into Mutable (lists)
+# and immutable such as (Tuples and Strings)
